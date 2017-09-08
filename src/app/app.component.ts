eb35c28d5a422137440d807b0acf5dcc36195ffd
Manideep Illendula <illendula.manideep@gmail.com>
2017-09-07 17:45:00 -0700

    Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
 
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBG-I6JtVuoDD6mvBuf800PNZr9-PjawDA',
+      authDomain: 'recipe-shopping-38df8.firebaseapp.co'
+    });
+  }
   onNavigate(feature: string) {
     this.loadedFeature = feature;
   }
